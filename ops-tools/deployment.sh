b660be33220cc26b2ad804c52bc08cf8301caab7
Dmitry Borisenko <borisenkodv@gmail.com>
2023-09-07 14:29:10 +0600

    VTD-8 Fix: unit file for delay service Add: more service deployment configuration Change: move delay service config outside jar

--- ops-tools/deployment.sh
+++ ops-tools/deployment.sh
@@ -1,3 +1,5 @@
+# install git
+
 # install jdk
 wget https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_linux-x64_bin.tar.gz -O openjdk19.tar.gz
 mkdir openjdk19
@@ -9,3 +11,21 @@ mv openjdk19 /usr/lib/jvm/openjdk19
 # Add service user
 useradd -M vtd
 usermod -L vtd
+
+# checkout vtd repo
+git clone
+
+# build delay service
+./gradlew :delay-service:build
+
+# install service
+chmod +x delay-service/build/libs/delay-service-0.0.1-SNAPSHOT.jar
+mkdir -p /opt/vtd/delay-service
+cp -R delay-service/config /opt/vtd/delay-service/config
+cp delay-service/build/libs/delay-service-0.0.1-SNAPSHOT.jar /opt/vtd/delay-service/delay-service.jar
+cp ops-tools/service/delay.service /etc/systemd/system/delay.service
+
+# start service
+systemctl daemon-reload
+systemctl start delay.service
+systemctl enable delay.service
