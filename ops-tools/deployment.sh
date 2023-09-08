6ffaa6604e36a939c173dfe22a7e026ce19baf90
Dmitry Borisenko <borisenkodv@gmail.com>
2023-09-08 15:31:22 +0600

    VTD-8 Fix: delay service deployment script so that it works in AWS

--- ops-tools/deployment.sh
+++ ops-tools/deployment.sh
@@ -1,22 +1,31 @@
+#!/bin/bash
+
 # install git
+yum install git -y
+#apt install git
 
 # install jdk
+mkdir vtd-test
+cd vtd-test
 wget https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_linux-x64_bin.tar.gz -O openjdk19.tar.gz
 mkdir openjdk19
 tar -xf openjdk19.tar.gz -C openjdk19 --strip-components 1
-
 mkdir -p /usr/lib/jvm
 mv openjdk19 /usr/lib/jvm/openjdk19
+export JAVA_HOME=/usr/lib/jvm/openjdk19
 
 # Add service user
 useradd -M vtd
 usermod -L vtd
 
 # checkout vtd repo
-git clone
+git clone https://github.com/dmitrybor/virtual-thread-demonstrator.git
+cd virtual-thread-demonstrator
+git checkout feature/VTD-8-service-deployment
 
 # build delay service
-./gradlew :delay-service:build
+./gradlew clean :delay-service:build -x :delay-service:test --no-daemon
+./gradlew --stop
 
 # install service
 chmod +x delay-service/build/libs/delay-service-0.0.1-SNAPSHOT.jar
