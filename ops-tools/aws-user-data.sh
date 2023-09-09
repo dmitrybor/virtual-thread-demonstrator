54202b334dd78febfae3b762d7ec65df980dc998
Dmitry Borisenko <borisenkodv@gmail.com>
2023-09-09 15:48:46 +0600

    VTD-8 Fix: delay service installation script

---
+++ ops-tools/aws-user-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install git
+yum install git -y
+#apt install git
+
+mkdir vtd-test
+cd vtd-test
+
+# checkout vtd repo
+git clone https://github.com/dmitrybor/virtual-thread-demonstrator.git
+cd virtual-thread-demonstrator
+git checkout feature/VTD-8-service-deployment
+export VTD_PROJECT_DIR=$(pwd)
+
+source ./ops-tools/install-delay.service.sh
