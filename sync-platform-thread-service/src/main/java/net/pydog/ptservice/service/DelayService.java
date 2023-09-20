107f886befe770b723e7920e7993a7853ded2df6
Dmitry Borisenko <borisenkodv@gmail.com>
2023-09-20 14:43:24 +0600

    VTD-14 Add: web layer for the sync platform thread app

---
+++ sync-platform-thread-service/src/main/java/net/pydog/ptservice/service/DelayService.java
@@ -0,0 +1,12 @@
+package net.pydog.ptservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DelayService {
+
+  public Integer delay(final Integer delayMs) {
+    return null;
+  }
+
+}
