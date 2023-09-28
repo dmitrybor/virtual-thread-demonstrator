b60ea45e7340ffe5a5d95ccf8568092eb065ddec
Dmitry Borisenko <borisenkodv@gmail.com>
2023-09-28 12:43:47 +0600

    VTD-16 Change: provide Sync Service start parameters via variables

--- ops-tools/install-sync-service.sh
+++ ops-tools/install-sync-service.sh
@@ -4,16 +4,16 @@ cd $VTD_PROJECT_DIR
 
 # install jdk
 source ./ops-tools/install-java.sh
-if [ $1 = 'virtual' ]
-then
-  export THREAD_TYPE='virtual'
-else
-  export THREAD_TYPE='platform'
-fi
+#if [ $1 = 'virtual' ]
+#then
+#  export THREAD_TYPE='virtual'
+#else
+#  export THREAD_TYPE='platform'
+#fi
 
 export SERVICE_NAME="$THREAD_TYPE"sync
-export SERVICE_PORT="$2"
-export DELAY_SERVICE_URL="$3"
+#export SERVICE_PORT="$2"
+#export DELAY_SERVICE_URL="$3"
 
 echo "Deploying $SERVICE_NAME service"
 export SERVICE_WORKING_DIRECTORY="/opt/vtd/$THREAD_TYPE-sync-service"
